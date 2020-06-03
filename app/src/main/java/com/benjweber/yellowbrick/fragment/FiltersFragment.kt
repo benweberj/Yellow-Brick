161c17755f120535e1f15fa4cc7c23579f250d2f
Chris Hogan <chrishogan17@outlook.com>
2020-06-02 23:50:53 -0700

    Add a filters fragment

---
+++ app/src/main/java/com/benjweber/yellowbrick/fragment/FiltersFragment.kt
@@ -0,0 +1,25 @@
+package com.benjweber.yellowbrick.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.benjweber.yellowbrick.R
+
+class FiltersFragment : Fragment() {
+
+    companion object {
+        val TAG: String = FiltersFragment::class.java.simpleName
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_filters , container, false)
+    }
+}
